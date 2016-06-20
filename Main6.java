2926f591debb16bb8521ee28fb255e71cb6f95f1
Mr-DengKai <964809903@qq.com>
2016-06-20 22:11:12 +0800

    add WriteFile.Main.java

---
+++ Main6.java
@@ -0,0 +1,13 @@
+package com.orilore.dk.test;
+
+import java.io.Writer;
+
+public class Main6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WriteFile str = new WriteFile();
+str.sendMsg("F:\\test.txt");
+	}
+
+}
